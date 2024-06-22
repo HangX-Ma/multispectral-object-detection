6910cb82ac3c1b0335e56e6a4e054d1e8f21dbbf
HangX-Ma <m-contour@qq.com>
2024-06-22 18:58:54 +0800

    fix: fix pbox transpose mistake in 'bbox_loss'

--- utils/loss.py
+++ utils/loss.py
@@ -129,7 +129,7 @@ class ComputeLoss:
                 pxy = ps[:, :2].sigmoid() * 2. - 0.5
                 pwh = (ps[:, 2:4].sigmoid() * 2) ** 2 * anchors[i]
                 pbox = torch.cat((pxy, pwh), 1)  # predicted box
-                iou = bbox_iou(pbox, tbox[i], WIoU=True, scale=True)  # iou(prediction, target)
+                iou = bbox_iou(pbox.T, tbox[i], WIoU=True, scale=True)  # iou(prediction, target)
 
                 if isinstance(iou, tuple):
                     if len(iou) == 2:
